660f139bbd909dc152cc272d776924831e6d8ba7
GMGustafson <gustafsg@email.sc.edu>
2024-09-14 23:23:28 -0400

    11:23

--- csce242/assignments/05_javascript/script.js
+++ csce242/assignments/05_javascript/script.js
@@ -1,12 +1,17 @@
 
     /*slider*/ 
         var slider = document.getElementById("myRange");
-        output.innerHTML = slider.value; // Display the default slider value
+        output.innerHTML = slider.value; 
 
         slider.oninput = function() {
         output.innerHTML = this.value;}
 
+        slider-pic == output.innerHTML; 
+
+        
     /*counter*/ 
     myButton.onclick = () => {
-        document.getElementById("myButton").innerHTML = innerHTML ++;
+        document.getElementById("myButton").innerHTML ;;
+        const element = document.getElementById("myButton");
+        element.innerHTML = innerHTML + 1;
     }
