70e8619a63caccfb1fc9cc98fad6db1ab41c83a7
GMGustafson <gustafsg@email.sc.edu>
2024-09-14 22:49:19 -0400

    10:50pm

--- csce242/assignments/05_javascript/script.js
+++ csce242/assignments/05_javascript/script.js
@@ -1,16 +1,12 @@
 
-        let count = 0; // Variable to track the count
-
-        // Set up the button click event listener
-        document.getElementById("myButton").onclick = () => {
-            count += 1; // Increment the count
-            document.getElementById("clickme").innerHTML = count; // Update the h1 content
-        }
-
+    /*slider*/ 
         var slider = document.getElementById("myRange");
-        var output = document.getElementById("demo");
         output.innerHTML = slider.value; // Display the default slider value
 
-        // Update the current slider value (each time you drag the slider handle)
         slider.oninput = function() {
         output.innerHTML = this.value;}
+
+    /*counter*/ 
+    myButton.onclick = () => {
+        document.getElementById("myButton").innerHTML = innerHTML ++;
+    }
