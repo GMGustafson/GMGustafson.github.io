b0743300c732fd7c3cb724c38b3ce4a5aa6c2ccc
GMGustafson <gustafsg@email.sc.edu>
2024-09-15 21:41:36 -0400

    9:41

--- csce242/assignments/05_javascript/script.js
+++ csce242/assignments/05_javascript/script.js
@@ -1,16 +1,16 @@
 
     // Slider
-document.getElementById("slider").value = 0; // Initial value of slider
+document.getElementById("slider").value = 0; 
 
 document.getElementById("slider").oninput = () => {
-    console.log("Check"); // Check input function
     val = document.getElementById("slider").value * 1.25;
-    document.getElementById("slide-box").style.left = val + "px";
+    document.getElementById("slider-pic").style.left = val + "px";
 }
         
     /*counter*/ 
     let taps = 0; 
-    document.getElementById("myButton").onclick = () => {
-        taps +=1; 
+    document.getElementById("counter").onclick = () => 
+    {
+        taps += 1; 
         document.getElementById("myButton").innerHTML = taps;
-    }
+    };
