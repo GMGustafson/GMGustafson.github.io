9d4e70beb998064dcb719d1a3a9cded00607e863
GMGustafson <gustafsg@email.sc.edu>
2024-09-18 20:19:14 -0400

    8:19

--- csce242/exercises/08_js_evenmore/script.js
+++ csce242/exercises/08_js_evenmore/script.js
@@ -1,2 +1,17 @@
 /* when the hamburger is clicked, toggle between showing and hiding the nav items*/ 
 
+//document.getElementbyId("toggle-nav")
+
+document.querySelector("#toggle-nav").onclick = () => 
+{ 
+    document.getElementById("nav-items").classList.toggle("hidden-small");  
+};
+
+/* dont go to links desititation when clicked */ 
+document.getElementById("click-link").onclick = (event) => 
+{ 
+    event.preventDefault(); //dont go to the links desination 
+    console.log("you clicked a link"); 
+};
+
+
