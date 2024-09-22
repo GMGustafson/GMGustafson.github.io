0c33ea5d7ed429befb20f03c7c2c8fd97c1360e8
GMGustafson <gustafsg@email.sc.edu>
2024-09-22 18:37:49 -0400

    6:37

--- csce242/assignments/06_/script.js
+++ csce242/assignments/06_/script.js
@@ -1,27 +1,52 @@
+
+
+document.querySelector("#toggle-nav").onclick = () => {
+    document.getElementById("nav-items").classList.toggle("hidden-small");
+};
+
+
+
 /* Color SLider */
+
+document.getElementById("myslider").vlaue = 0; 
+
+document.getElementById("myslider").oninput = () => {
+    let color = document.getElementById("myslider").value; 
+    const message = document.getElementById("message");
+    document.getElementById("exercise-1").style.backgrounColor = "rgb(" + color + ", 0, 0)";
+    
+
+    if(color >= 50 && color < 155){
+        message.innerHTMl = "Fun!"
+    }
+    else if(color >= 155 && color < 255){
+        message.innerHTMl = "Oh Really Fun!"
+    }
+    else if (val == 255) 
+    {
+         message.innerHTMl = "SO AWESOME!"
+    }
+   
+}
 
 
 
 /* Picture Chooser*/ 
 document.getElementById("exercise.2").onclick = (event) => 
 {
-    const button = event.target; 
-    const img = document.getElementById("img");
+    const button = event.target;
 
     if (button == "small") 
     { 
-        console.log("hi hi");
-        //img.src="https://picsum.photos/100";
+        document.getElementById.src = "https://picsum.photos/100";
     }
-    if (button == "medium") 
+    else if (button == "medium") 
     { 
-        console.log("hiiiii hi");
-        //img.src="https://picsum.photos/300";
+        document.getElementById.src=img.src = "https://picsum.photos/300";
     }
-    if (button == "large") 
+    else if (button == "large") 
     { 
-        console.log("hi hiii");
-        //img.src="https://picsum.photos/500";
+        document.getElementById.src=img.src = "https://picsum.photos/500";
     }
    
 }
