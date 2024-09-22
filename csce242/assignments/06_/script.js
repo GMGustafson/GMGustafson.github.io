3d09cf6bf866543f064a7469bab08237dda97e2c
GMGustafson <gustafsg@email.sc.edu>
2024-09-22 19:08:10 -0400

    7:08

--- csce242/assignments/06_/script.js
+++ csce242/assignments/06_/script.js
@@ -10,7 +10,12 @@ document.querySelector("#toggle-nav").onclick = () => {
 
 document.getElementById("myslider").vlaue = 0; 
 
-document.getElementById("myslider").oninput = () => {
+document.getElementById("myslider").onchange = (e) => {
+    document.querySelector(":root").style.setProperty("--box-color", e.target.value);
+}
+
+document.getElementById("myslider").oninput= () => 
+{
     let color = document.getElementById("myslider").value; 
     const message = document.getElementById("message");
     document.getElementById("exercise-1").style.backgrounColor = "rgb(" + color + ", 0, 0)";
@@ -22,17 +27,15 @@ document.getElementById("myslider").oninput = () => {
     else if(color >= 155 && color < 255){
         message.innerHTMl = "Oh Really Fun!"
     }
-    else if (val == 255) 
+    else if (color == 255) 
     {
          message.innerHTMl = "SO AWESOME!"
     }
    
 }
 
-
-
 /* Picture Chooser*/ 
-document.getElementById("exercise.2").onclick = (event) => 
+document.getElementById("exercise-2").onclick = (event) => 
 {
     const button = event.target;
 
