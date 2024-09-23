280a3673d5eea2b84fd38a80f2f2feaf683fee07
GMGustafson <gustafsg@email.sc.edu>
2024-09-22 21:27:24 -0400

    9:27

--- csce242/assignments/06_/script.js
+++ csce242/assignments/06_/script.js
@@ -6,30 +6,24 @@ document.querySelector("#toggle-nav").onclick = () => {
 
 
 
+
 /* Color SLider */
-
-document.getElementById("myslider").vlaue = 0; 
-
-document.getElementById("myslider").onchange = (e) => {
-    document.querySelector(":root").style.setProperty("--box-color", e.target.value);
-}
-
 document.getElementById("myslider").oninput= () => 
 {
     let color = document.getElementById("myslider").value; 
     const message = document.getElementById("message");
-    document.getElementById("exercise-1").style.backgrounColor = "rgb(" + color + ", 0, 0)";
+    document.getElementById("exercise-1").style.backgroundColor = "rgb(" + color + ", 0, 0)";
     
 
     if(color >= 50 && color < 155){
-        message.innerHTMl = "Fun!"
+        message.innerHTML = "Fun!";
     }
     else if(color >= 155 && color < 255){
-        message.innerHTMl = "Oh Really Fun!"
+        message.innerHTML = "Oh Really Fun!";
     }
     else if (color == 255) 
     {
-         message.innerHTMl = "SO AWESOME!"
+        message.innerHTML = "SO AWESOME!";
     }
    
 }
@@ -39,17 +33,18 @@ document.getElementById("exercise-2").onclick = (event) =>
 {
     const button = event.target;
 
-    if (button == "small") 
+    
+    if (button.id == "small") 
     { 
-        document.getElementById.src = "https://picsum.photos/100";
+        document.getElementById("image").src = "https://picsum.photos/100";
     }
-    else if (button == "medium") 
+    else if (button.id == "medium") 
     { 
-        document.getElementById.src=img.src = "https://picsum.photos/300";
+        document.getElementById("image").src = "https://picsum.photos/200";
     }
-    else if (button == "large") 
+    else if (button.id == "large") 
     { 
-        document.getElementById.src=img.src = "https://picsum.photos/500";
+        document.getElementById("image").src = "https://picsum.photos/300";
     }
    
 }
