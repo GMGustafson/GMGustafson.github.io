acbdbe13b2b1e7579a321738d5884d6981bb7afa
GMGustafson <gustafsg@email.sc.edu>
2024-10-01 16:17:42 -0400

    4:17

--- csce242/assignments/08_Arrays/script.js
+++ csce242/assignments/08_Arrays/script.js
@@ -14,18 +14,17 @@
 const container = document.querySelector("#pictures-list");
 
 for(let name in pictures){
-  let tr = document.createElement("tr");
-
-  const td1 = document.createElement("td");
+  let div = document.createElement("div");
+  div.classList.add("pic-div");
+  
   let img = document.createElement("img"); 
   img.src = name; 
-  td1.appendChild(img); 
-  tr.append(td1);
 
-  
+  div.appendChild(img); 
+
   img.onclick = () => {
     document.getElementById("image-name").innerHTML = pictures[name]; 
 }
-container.appendChild(tr);
+container.appendChild(div);
 
 }
