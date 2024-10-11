ba39f4fa512532e3db8d7577fb95bd94197bb272
GMGustafson <gustafsg@email.sc.edu>
2024-10-10 23:43:31 -0400

    11:43

--- csce242/exercises/12_json/part3/script.js
+++ csce242/exercises/12_json/part3/script.js
@@ -1,1 +1,46 @@
-//"https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+const getCocktails = async() => {
+    try {
+        return (await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a")).json();
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+const showCocktailSection = async() => {
+    const cocktail = await getCocktails();
+    const drinks = cocktail.drinks; 
+    
+    drinks.forEach((cocktail)=>{
+        document.getElementById("cocktails-section").append(getCocktailSection(cocktail));
+    });
+};
+
+const getCocktailSection = (cocktail) => {
+    const section = document.createElement("section");
+
+    const h3 = document.createElement("h3");
+    h3.innerHTML = cocktail.strDrink;
+    section.append(h3);
+      
+    const image = document.createElement("img");
+    image.src = cocktail.strDrinkThumb; 
+    section.appendChild(image);
+
+    
+
+    image.onclick = () => {
+        const instructions = document.createElement("p");
+        instructions.innerHTML = cocktail.strInstructions;
+        section.append(instructions);
+    };
+    return section;
+
+}; 
+
+
+
+
+
+showCocktailSection(); 
+
+
